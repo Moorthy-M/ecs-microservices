d327e6c220a86efa1ddecc5b8a4098ef24c3e7d9
Moorthy-M <moorthy2652002@gmail.com>
2026-02-07 16:24:46 +0000

    Service Roles Updated

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -5,3 +5,19 @@ output "platform_ci_role_arn" {
 output "platform_cd_role_arn" {
   value = module.iam.platform_cd_role_arn
 }
+
+output "service_ci_role_arn" {
+  value = module.iam.service_ci_role_arn
+}
+
+output "service_cd_role_arn" {
+  value = module.iam.service_cd_role_arn
+}
+
+output "ecs_execution_role_arn" {
+  value = module.iam.ecs_execution_role_arn
+}
+
+output "ecs_task_role_arns" {
+  value = module.iam.ecs_task_role_arns
+}
